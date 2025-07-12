06fbf67646f5b9ff7b2be7f8193ade65bddae144
JFH11 <raditya.arjun64@smk.belajar.id>
2025-07-12 19:47:19 +0700

    Update server-ranziro.js

--- server-ranziro.js
+++ server-ranziro.js
@@ -3,24 +3,28 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const apps = express();
 
+// Tambah semua header keamanan di sini
 apps.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store'); // Tidak menyimpan cache
+    res.setHeader("Cache-Control", "no-store");
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+    res.setHeader("Permissions-Policy", "geolocation=(), microphone=()");
+    res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
     next();
 });
 
 apps.use(bodyParser.json());
 apps.use(express.static(path.join(__dirname, 'public')));
 
-// dinamis route
 const router = [
-    { path: '/mlbb', file: 'index.html' },
-    { path: '/ff', file: 'ff.html' },
+    { path: '/mobile-legends', file: 'index.html' },
+    { path: '/free-fire', file: 'ff.html' },
     { path: '/akun', file: 'akun.html' },
     { path: '/terms', file: 'terms.html' },
     { path: '/privacy-policy', file: 'privacy-policy.html' }
 ];
 
-// Membuat rute secara dinamis berdasarkan konfigurasi
 router.forEach(route => {
     apps.get(route.path, (req, res) => {
         res.sendFile(path.join(__dirname, 'public', route.file));
